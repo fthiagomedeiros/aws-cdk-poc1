a0bcaae95ae0e7706131edcb65be3b1268da7aee
Francisco Thiago <fthiagomedeiros@gmail.com>
2024-06-05 23:30:04 +0200

    CDK Initial Project

--- src/main/java/com/myorg/MyCustomCdkApp.java
+++ src/main/java/com/myorg/MyCustomCdkApp.java
@@ -1,40 +1,18 @@
 package com.myorg;
 
 import software.amazon.awscdk.App;
-import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
-import java.util.Arrays;
-
 public class MyCustomCdkApp {
+
     public static void main(final String[] args) {
         App app = new App();
 
         new MyCustomCdkStack(app, "MyCustomCdkStack", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
+            .build());
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
-                .env(Environment.builder()
-                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                        .region(System.getenv("CDK_DEFAULT_REGION"))
-                        .build())
-                */
-
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                .env(Environment.builder()
-                        .account("123456789012")
-                        .region("us-east-1")
-                        .build())
-                */
-
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                .build());
+        new MySecondCdkStack(app, "MySecondCdkStack", StackProps.builder()
+            .build());
 
         app.synth();
     }
--- src/main/java/com/myorg/MyCustomCdkStack.java
+++ src/main/java/com/myorg/MyCustomCdkStack.java
@@ -1,12 +1,12 @@
 package com.myorg;
 
+import software.amazon.awscdk.services.s3.Bucket;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
 
 public class MyCustomCdkStack extends Stack {
+
     public MyCustomCdkStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
@@ -15,10 +15,8 @@ public class MyCustomCdkStack extends Stack {
         super(scope, id, props);
 
         // The code that defines your stack goes here
-
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "MyCustomCdkQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        Bucket.Builder.create(this, "my-custom-cdk-bucket")
+            .versioned(true)
+            .build();
     }
 }
