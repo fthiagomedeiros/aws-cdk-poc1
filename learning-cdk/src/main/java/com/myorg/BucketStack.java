6b656f22cf71a03511efdc9e60941d07075f435e
Francisco Thiago <fthiagomedeiros@gmail.com>
2024-06-19 00:49:51 +0200

    Including a Level 1 CDK - S3 Bucket creation

--- learning-cdk/src/main/java/com/myorg/BucketStack.java
+++ learning-cdk/src/main/java/com/myorg/BucketStack.java
@@ -1,8 +1,10 @@
 package com.myorg;
 
+import software.amazon.awscdk.CfnDeletionPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.s3.CfnBucket;
+import software.amazon.awscdk.services.s3.CfnBucketProps;
 import software.constructs.Construct;
 
 public class BucketStack extends Stack {
@@ -10,7 +12,10 @@ public class BucketStack extends Stack {
     public BucketStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        CfnBucket bucket = new CfnBucket(this, "my-custom-bucket-fthiagom");
+        CfnBucket bucket = new CfnBucket(this, "my-custom-bucket-fthiagom", CfnBucketProps.builder().build());
+
+        bucket.getCfnOptions().setDeletionPolicy(CfnDeletionPolicy.RETAIN_EXCEPT_ON_CREATE);
+
     }
 
 
